afcf3290cee3ea550ae1a14e7c4a24f906db007f
Bence Knáb <benceknab.iac@gmail.com>
2019-01-26 17:45:58 +0100

    Fix deprecation issues

--- backend/start.js
+++ backend/start.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
 const app = require('./app');
 
-// Import environmental variables
-require('dotenv').config({ path: 'variables.env' });
+dotenv.config({ path: 'variables.env' });
 
-// Connect to our Database and handle any bad connections
-mongoose.connect(process.env.DATABASE);
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 mongoose.connection.on('error', (err) => {
   console.error(`${err.message}`);
@@ -14,7 +14,6 @@ mongoose.connection.on('error', (err) => {
 // Import models
 // require('./models/User');
 
-// Start the app!
 app.set('port', process.env.PORT || 7777);
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running on PORT ${server.address().port}`);
