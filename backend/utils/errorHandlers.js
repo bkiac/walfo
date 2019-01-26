6a577e3a0759cd80dfad3d1525544173d28f6b62
Bence Knáb <benceknab.iac@gmail.com>
2019-01-26 17:25:41 +0100

    Add utils folder and refactor `handleErrors`

--- backend/utils/errorHandlers.js
+++ backend/utils/errorHandlers.js
@@ -1,8 +1,5 @@
 /*
   Catch Errors Handler
-
   Instead of using try-catch for async/await, a function is wrapped in catchErrors()
 */
-exports.catchErrors = fn => function (req, res, next) {
-  return fn(req, res, next).catch(next);
-};
+exports.catchErrors = fn => (req, res, next) => fn(req, res, next).catch(next);
