992208a73eda09c1dcf1cfe2d4f0101df77fe127
Bence Knáb <benceknab.iac@gmail.com>
2019-02-09 19:29:33 +0100

    Refactor routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 
+const authRoutes = require('./authRoutes');
 const userRoutes = require('./userRoutes');
+const transactionRoutes = require('./transactionRoutes');
+const tagsRoutes = require('./tagsRoutes');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send({
-    something: 'happened',
-  });
-});
-
-router.use('/user', userRoutes);
+router.use('/auth', authRoutes);
+router.use('/users', userRoutes);
+router.use('/transactions', transactionRoutes);
+router.use('/tags', tagsRoutes);
 
 module.exports = router;
