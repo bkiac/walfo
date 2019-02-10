979ce488956c89aaad48b0b9ff18de9cef7f86b5
Bence Knáb <benceknab.iac@gmail.com>
2019-02-10 10:23:17 +0100

    Remove unnecessary tags routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,13 +3,11 @@ const express = require('express');
 const authRoutes = require('./authRoutes');
 const userRoutes = require('./userRoutes');
 const transactionRoutes = require('./transactionRoutes');
-const tagsRoutes = require('./tagsRoutes');
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 router.use('/transactions', transactionRoutes);
-router.use('/tags', tagsRoutes);
 
 module.exports = router;
