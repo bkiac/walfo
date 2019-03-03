aec0b3697c96fe4cb711055f3d344b78db84ad61
Bence Knáb <benceknab.iac@gmail.com>
2019-03-03 08:26:41 +0100

    Add jest for tests, and add a simple test for chunk helper

---
+++ backend/test/utils/helpers.test.js
@@ -0,0 +1,26 @@
+/* eslint-env jest */
+const helpers = require('../../src/utils/helpers');
+
+describe('helpers', () => {
+  describe('chunk', () => {
+    describe('array.length > chunkSize', () => {
+      it('should return a chunked array, with chunks in the given size', () => {
+        const array = [1, 2, 3, 4];
+        const chunkSize = 2;
+        const chunks = helpers.chunk(array, chunkSize);
+        expect(chunks).toHaveLength(Math.ceil(array.length / chunkSize));
+        chunks.forEach(ch => expect(ch).toHaveLength(chunkSize));
+      });
+    });
+
+    describe('array.length <= chunkSize', () => {
+      it('should return the whole array in the first chunk', () => {
+        const array = [1, 2, 3, 4];
+        const chunkSize = 6;
+        const chunks = helpers.chunk(array, chunkSize);
+        expect(chunks).toHaveLength(Math.ceil(array.length / chunkSize));
+        chunks.forEach(ch => expect(ch).toHaveLength(array.length));
+      });
+    });
+  });
+});
