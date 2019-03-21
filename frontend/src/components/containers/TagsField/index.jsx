27662359ec5df615f77dca3c7eab511f883e3a24
Bence Knáb <benceknab.iac@gmail.com>
2019-03-21 19:16:11 +0100

    Add tags input field

---
+++ frontend/src/components/containers/TagsField/index.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { Paper, TextField } from '@material-ui/core';
+import { uniq } from 'lodash';
+import * as PropTypes from 'prop-types';
+import style from './style.module.scss';
+import { Tag } from '../../views';
+
+function TagsField({ initialTags, onChange }) {
+  const [input, setInput] = useState('');
+  const [tags, setTags] = useState(initialTags);
+
+  function change(newTags) {
+    setTags(newTags);
+    onChange(newTags);
+  }
+
+  function createTag(newTag) {
+    const newTags = uniq([...tags, newTag]);
+    change(newTags);
+  }
+
+  function removeTag(tagToBeRemoved) {
+    const newTags = tags.filter(t => t !== tagToBeRemoved);
+    change(newTags);
+  }
+
+  return (
+    <div>
+      <Paper className={style.padding}>
+        {tags.map(t =>
+          initialTags.includes(t) ? (
+            <Tag className={style.margin} key={t} tag={t} />
+          ) : (
+            <Tag className={style.margin} key={t} tag={t} onDelete={() => removeTag(t)} />
+          ),
+        )}
+      </Paper>
+      <TextField
+        fullWidth
+        margin="normal"
+        value={input}
+        onChange={e => setInput(e.target.value)}
+        onKeyDown={e => {
+          const key = e.which || e.keyCode;
+          if (key === 9) {
+            createTag(e.target.value);
+            setInput('');
+          }
+        }}
+      />
+    </div>
+  );
+}
+
+TagsField.propTypes = {
+  initialTags: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default TagsField;
