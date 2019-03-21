f17dc0e97bd7a1750d45439934a04acf72b57c3e
Bence Knáb <benceknab.iac@gmail.com>
2019-03-21 19:17:58 +0100

    Add dashboard new components to dashboard

--- frontend/src/components/containers/Dashboard/index.jsx
+++ frontend/src/components/containers/Dashboard/index.jsx
@@ -1,53 +1,92 @@
 import React, { useState } from 'react';
-import { Button, Drawer, List, ListItem, ListItemText } from '@material-ui/core';
+import {
+  Button,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  Grid,
+} from '@material-ui/core';
 import { useApiOnMount, useLogout } from '../../../hooks';
-import { portfolioApi } from '../../../api';
+import { coinsApi, portfolioApi } from '../../../api';
 import { Spinner } from '../../views';
 import Portfolio from '../Portfolio';
+import TransactionForm from '../TransactionForm';
+import { CoinsProvider } from '../../providers';
 
 function Dashboard() {
   const logout = useLogout();
-  const [isDrawOpen, setIsDrawOpen] = useState(false);
-  const portfolios = useApiOnMount(portfolioApi.getPortfolioNames);
-  const [selectedPortfolioIndex, setSelectedPortfolioIndex] = useState(0);
 
+  const [isDrawOpen, setIsDrawOpen] = useState(false);
   function openDrawer() {
     setIsDrawOpen(true);
   }
-
   function closeDrawer() {
     setIsDrawOpen(false);
   }
 
-  if (portfolios.isLoading || portfolios.hasError) {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  function openDialog() {
+    setIsDialogOpen(true);
+  }
+  function closeDialog() {
+    setIsDialogOpen(false);
+  }
+
+  const portfolios = useApiOnMount(portfolioApi.getPortfolioNames);
+  const [selectedPortfolioIndex, setSelectedPortfolioIndex] = useState(0);
+
+  const coinList = useApiOnMount(coinsApi.getCoinList);
+
+  if (portfolios.isLoading || portfolios.hasError || coinList.isLoading) {
     return <Spinner />;
   }
   return (
-    <>
-      <Drawer open={isDrawOpen} onClose={closeDrawer}>
-        <List>
-          {portfolios.data.map((p, i) => (
-            <ListItem
-              key={p}
-              selected={i === selectedPortfolioIndex}
-              onClick={() => setSelectedPortfolioIndex(i)}
-            >
-              <ListItemText primary={p} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
+    <CoinsProvider initialCoins={coinList.data}>
+      <>
+        <Drawer open={isDrawOpen} onClose={closeDrawer}>
+          <List>
+            {portfolios.data.map((p, i) => (
+              <ListItem
+                key={p}
+                selected={i === selectedPortfolioIndex}
+                onClick={() => setSelectedPortfolioIndex(i)}
+              >
+                <ListItemText primary={p} />
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
 
-      <Button variant="contained" color="primary" onClick={logout}>
-        Logout
-      </Button>
+        <Dialog open={isDialogOpen} onClose={closeDialog} fullWidth maxWidth="sm">
+          <DialogTitle>New Transaction</DialogTitle>
+          <DialogContent>
+            <TransactionForm portfolio={portfolios.data[selectedPortfolioIndex]} />
+          </DialogContent>
+        </Dialog>
 
-      <Button variant="contained" color="primary" onClick={openDrawer}>
-        open fiók
-      </Button>
+        <Grid item>
+          <Grid container direction="row" justify="center" alignItems="flex-start">
+            <Button variant="contained" color="primary" onClick={logout}>
+              Logout
+            </Button>
 
-      <Portfolio name={portfolios.data[selectedPortfolioIndex]} />
-    </>
+            <Button variant="contained" color="primary" onClick={openDrawer}>
+              open fiók
+            </Button>
+
+            <Button variant="contained" color="primary" onClick={openDialog}>
+              new tx
+            </Button>
+          </Grid>
+        </Grid>
+
+        <Portfolio name={portfolios.data[selectedPortfolioIndex]} />
+      </>
+    </CoinsProvider>
   );
 }
 
--- frontend/src/components/containers/index.js
+++ frontend/src/components/containers/index.js
@@ -2,3 +2,6 @@ export { default as LoginForm } from './LoginForm';
 export { default as RegisterForm } from './RegisterForm';
 export { default as Dashboard } from './Dashboard';
 export { default as Portfolio } from './Portfolio';
+export { default as TransactionForm } from './TransactionForm';
+export { default as TagsField } from './TagsField';
+export { default as CoinField } from './CoinField';
