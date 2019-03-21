aa7e6bf125c559d3f06cc16f8a384ff7b9696239
Bence Knáb <benceknab.iac@gmail.com>
2019-03-21 19:11:52 +0100

    Refactor contexts and add coin context

--- frontend/src/contexts/index.js
+++ frontend/src/contexts/index.js
@@ -1,2 +1,4 @@
-// eslint-disable-next-line import/prefer-default-export
-export { default as UserContext } from './UserContext';
+import React from 'react';
+
+export const UserContext = React.createContext();
+export const CoinsContext = React.createContext();
