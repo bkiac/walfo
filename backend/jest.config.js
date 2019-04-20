18af50ed01fd0c2c428fa5867729d707c1f1cf4f
Bence Knáb <benceknab.iac@gmail.com>
2019-04-20 14:09:09 +0200

    Change timeout

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -123,7 +123,7 @@ module.exports = {
   setupFiles: ['dotenv/config', './test/mongoose.js', './test/axios.js'],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ['./test/framework.js'],
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   // snapshotSerializers: [],
