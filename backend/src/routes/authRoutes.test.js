8f9f885c05166fb312ba8a454292bb0304e1ae8a
Bence Knáb <benceknab.iac@gmail.com>
2019-04-20 12:08:53 +0200

    Add tests for login

--- backend/src/routes/authRoutes.test.js
+++ backend/src/routes/authRoutes.test.js
@@ -1,99 +1,127 @@
 /* eslint-env jest */
-const mongoose = require('mongoose');
-const axios = require('axios');
-const app = require('../app');
-require('../models/User');
-
-const User = mongoose.model('User');
-let axiosInstance;
-
-beforeAll(async () => {
-  await mongoose.connect(`${process.env.DATABASE}_test`, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-  });
-  mongoose.set('useFindAndModify', false);
-  mongoose.Promise = global.Promise;
-
-  const server = app.listen(0, '0.0.0.0', () => {
-    const { address, port } = server.address();
-    axiosInstance = axios.create({
-      baseURL: `http://${address}:${port}/api/auth`,
-    });
-    app.emit('started');
-  });
-});
-
-afterAll(async () => {
-  await mongoose.disconnect();
-});
+const { axios } = global;
 
 describe('authRoutes', () => {
-  beforeAll(done => {
-    app.on('started', done);
-  });
+  const registerRoute = '/auth/register';
+  const loginRoute = '/auth/login';
 
-  afterAll(async () => {
-    afterAll(async () => {
-      await User.deleteMany({});
+  describe(`POST ${registerRoute}`, () => {
+    it('should register', async () => {
+      try {
+        const res = await axios.post(registerRoute, {
+          email: 'any@email.com',
+          password: 'password',
+          confirmPassword: 'password',
+        });
+        expect(res.status).toBe(200);
+      } catch (err) {
+        throw Error(err);
+      }
+    });
+
+    it("shouldn't register without email", async () => {
+      try {
+        await axios.post(registerRoute, {
+          password: 'password',
+          confirmPassword: 'password',
+        });
+      } catch (err) {
+        expect(err.response.status).toBe(422);
+      }
+    });
+
+    it("shouldn't register without password", async () => {
+      try {
+        await axios.post(registerRoute, {
+          email: 'any@email.com',
+          confirmPassword: 'password',
+        });
+      } catch (err) {
+        expect(err.response.status).toBe(422);
+      }
+    });
+
+    it("shouldn't register without confirm password", async () => {
+      try {
+        await axios.post(registerRoute, {
+          email: 'any@email.com',
+          password: 'password',
+        });
+      } catch (err) {
+        expect(err.response.status).toBe(422);
+      }
+    });
+
+    it("shouldn't register without matching passwords", async () => {
+      try {
+        await axios.post(registerRoute, {
+          email: 'any@email.com',
+          password: 'password',
+          confirmPassword: 'differentPassword',
+        });
+      } catch (err) {
+        expect(err.response.status).toBe(422);
+      }
     });
   });
 
-  it('should register', async () => {
-    try {
-      const res = await axiosInstance.post('/register', {
-        email: 'any@email.com',
-        password: 'password',
-        confirmPassword: 'password',
-      });
-      expect(res.statusCode).toBe(200);
-    } catch (err) {
-      // Shouldn't throw error
-    }
-  });
+  describe(`POST ${loginRoute}`, () => {
+    const email = 'login@test.com';
+    const password = 'password';
 
-  it("shouldn't register without email", async () => {
-    try {
-      await axiosInstance.post('/register', {
-        password: 'password',
-        confirmPassword: 'password',
-      });
-    } catch (err) {
-      expect(err.response.status).toBe(422);
-    }
-  });
+    beforeAll(async () => {
+      try {
+        await axios.post(registerRoute, {
+          email,
+          password,
+          confirmPassword: password,
+        });
+      } catch (err) {
+        throw Error(err);
+      }
+    });
 
-  it("shouldn't register without password", async () => {
-    try {
-      await axiosInstance.post('/register', {
-        email: 'any@email.com',
-        confirmPassword: 'password',
-      });
-    } catch (err) {
-      expect(err.response.status).toBe(422);
-    }
-  });
+    it('should login', async () => {
+      try {
+        const res = await axios.post(loginRoute, {
+          email,
+          password,
+        });
+        expect(res.status).toBe(200);
+      } catch (err) {
+        throw Error(err);
+      }
+    });
 
-  it("shouldn't register without confirm password", async () => {
-    try {
-      await axiosInstance.post('/register', {
-        email: 'any@email.com',
-        password: 'password',
-      });
-    } catch (err) {
-      expect(err.response.status).toBe(422);
-    }
-  });
+    it("shouldn't login without email", async () => {
+      try {
+        await axios.post(loginRoute, {
+          password,
+        });
+      } catch (err) {
+        expect(err.response.status).toBe(401);
+      }
+    });
 
-  it("shouldn't register without matching passwords", async () => {
-    try {
-      await axiosInstance.post('/register', {
-        email: 'any@email.com',
-        password: 'password',
-        confirmPassword: 'differentPassword',
-      });
-    } catch (err) {
-      expect(err.response.status).toBe(422);
-    }
+    it("shouldn't login without password", async () => {
+      try {
+        await axios.post(loginRoute, {
+          email,
+        });
+      } catch (err) {
+        expect(err.response.status).toBe(401);
+      }
+    });
+
+    it("shouldn't login with wrong password", async () => {
+      try {
+        await axios.post(loginRoute, {
+          email,
+          password: 'wrongPassword',
+        });
+      } catch (err) {
+        expect(err.response.status).toBe(401);
+      }
+    });
   });
 });
