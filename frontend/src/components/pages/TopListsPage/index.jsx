09dea1f9d70ed04b091c7dc08ed277c78478cb11
Bence Knáb <benceknab.iac@gmail.com>
2019-04-25 10:29:48 +0200

    Fix negative margin

--- frontend/src/components/pages/TopListsPage/index.jsx
+++ frontend/src/components/pages/TopListsPage/index.jsx
@@ -7,19 +7,21 @@ function TopListsPage() {
     <>
       <BrowseNav />
 
-      <Grid container direction="row" justify="center" alignItems="center" spacing={16}>
-        <Grid item xs={12} md={4}>
-          <SearchCoins />
-        </Grid>
+      <div style={{ padding: '0 16px' }}>
+        <Grid container direction="row" justify="center" alignItems="center" spacing={16}>
+          <Grid item xs={12} md={4}>
+            <SearchCoins />
+          </Grid>
 
-        <Grid item xs={12} md={4}>
-          <TopMarketCapCoins />
-        </Grid>
+          <Grid item xs={12} md={4}>
+            <TopMarketCapCoins />
+          </Grid>
 
-        <Grid item xs={12} md={4}>
-          <TopVolumeCoins />
+          <Grid item xs={12} md={4}>
+            <TopVolumeCoins />
+          </Grid>
         </Grid>
-      </Grid>
+      </div>
     </>
   );
 }
